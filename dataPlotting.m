e44deab6912564ed60a14d19cc870bcf462f3cb3
aidanf32-stack <aidanf32@vt.edu>
2026-04-01 15:20:42 -0400

    Add files via upload

--- dataPlotting.m
+++ dataPlotting.m
@@ -1,6 +1,7 @@
+%% Testing 
 clear; clc; close all
 
-sp = serialport("Com1", 115200); % reads data from serial connection
+sp = serialport("COM4", 115200); % reads data from serial connection
 configureTerminator(sp,"LF"); % sets terminator to LF
 
 % assume data comes as time, pressure, Tp1 - 6, Tin, Tout
@@ -22,7 +23,7 @@ Ti = plot(sp2, nan, nan); % internal
 xlabel(sp2, 'Time (s)');
 ylabel(sp2, 'Temperature (°C)');
 title(sp2, 'Internal and External Ambient Temperature');
-legend(sp2, 'Internal', 'External');
+legend(sp2, 'External', 'Internal');
 hold(sp2, "off");
 
 sp3 = subplot(2,2,3); % 3rd subplot --> pressure
@@ -43,6 +44,7 @@ hold(sp4, "off");
 
 % placeholder vectors (updated continuously in loop)
 
+data = [];
 timestamp = []; % elapsed time (s)
 pressure = []; % enclosure ambient temp ([UNIT TBD])
 panel1_temp = []; % panel temp (deg C)
@@ -60,8 +62,8 @@ while true % infinite loop, runs as long as data comes in
 
     line = readline(sp); % reads a single line from serial connection
     itr = str2double(split(line, ",")); % converts line to numerical values
-    data = [data; str2double(split(line, ","))]; % converts line to numerical values stored in a matrix
-    
+    data = [data str2double(split(line, ","))]; % converts line to numerical values stored in a matrix
+
     % update variables with latest iteration
     timestamp(end+1) = itr(1);
     pressure(end+1) = itr(2);
@@ -74,14 +76,15 @@ while true % infinite loop, runs as long as data comes in
     T_intamb(end+1) = itr(9);
     T_outamb(end+1) = itr(10);
     mean_paneltemp(end+1) = sum(itr(3:8))/6; % mean temp calculation
-    
+
     % time derivative using a backwards difference approximation
     if length(timestamp) > 1
         dTdt(end+1) = (T_intamb(end) - T_intamb(end-1))/(timestamp(end) - timestamp(end-1));
+    % for initial data point w/ no 'backward' instance available
     else
-        dTdt(end) = 1; % for initial data point w/ no 'backward' instance available
+        dTdt = 1;
     end
-    
+
     % update plots with new data
     set(MPT, "XData", timestamp, "YData", mean_paneltemp); % subplot 1
     set(To, "XData", timestamp, "YData", T_outamb); % subplot 2
@@ -91,6 +94,71 @@ while true % infinite loop, runs as long as data comes in
 
     drawnow limitrate % display
 
-    % writematrix(data, "test_n_data.csv"); 
+    writematrix(data, "test_3_data.csv"); 
 
 end
+
+%% Post Processing
+clear; clc; close all
+
+ppd = readmatrix('3_31_Rising_data.csv');
+
+t = ppd(1,:)./1000;
+p = ppd(2,:);
+T_p1 = ppd(3,:);
+T_p2 = ppd(4,:);
+T_p3 = ppd(5,:);
+T_p4 = ppd(6,:);
+T_p5 = ppd(7,:);
+T_p6 = ppd(8,:);
+T_p_avg = (T_p1 + T_p2 + T_p3 + T_p4 + T_p5 + T_p6)/6;
+T_int = ppd(9,:);
+T_ext = ppd(10,:);
+
+subplot(2,2,1);
+hold on
+plot(t,T_p_avg);
+xlabel('Time (s)');
+ylabel('Temperature (degrees C)');
+title('Average Panel Temperature During Testing');
+hold off
+
+subplot(2,2,2);
+hold on
+plot(t,T_int);
+plot(t,T_ext);
+xlabel('Time (s)');
+ylabel('Temperature (degrees C)');
+legend('Internal Ambient', 'External Ambient',Location='southoutside');
+title('Ambient Temperature During Testing');
+hold off
+
+subplot(2,2,3);
+hold on
+plot(t,p);
+xlabel('Time (s)');
+ylabel('Pressure (psi)');
+title('Enclosure Ambient Pressure During Testing');
+hold off
+
+dTdt = [];
+for i = 1:(length(t)-1)
+    if i == 1
+        dTdt(i) = 1;
+    else
+        dTdt(i) = (T_int(i+1) - T_int(i))/(t(i+1) - t(i));
+    end
+end
+
+dTdt(end+1) = dTdt(end);
+
+subplot(2,2,4);
+hold on
+plot(t,dTdt);
+xlabel('Time (s)');
+ylabel('dT/dt (degrees C / s)');
+title('Temperature Time Derivative During Testing');
+hold off
+
+
+b = [t;dTdt];
